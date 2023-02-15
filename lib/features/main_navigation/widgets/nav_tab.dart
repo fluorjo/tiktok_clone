df3f68ee8c83e9e03c4197583f216809f1c749b9
fluorjo <fluorjo@naver.com>
2023-02-15 23:25:30 +0900

    6.5 Custom NavigationBar

---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  const NavTab(
+      {super.key,
+      required this.text,
+      required this.isSelected,
+      required this.icon,
+      required this.onTap});
+
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          //이거 색깔 지정 안해주면 아이콘 외 부분에 클릭 적용되는 거 안 먹힘. 아마 container를 불필요한 걸로 인식해서 그런 듯. 
+          color: Colors.black,
+          child: AnimatedOpacity(
+            opacity: isSelected ? 1 : 0.6,
+            duration: const Duration(milliseconds: 100),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
