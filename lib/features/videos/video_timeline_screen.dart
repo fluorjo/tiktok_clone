5c9c48def2ca86f9e390a4355e74294e78b598c9
fluorjo <fluorjo@naver.com>
2023-02-17 04:32:15 +0900

    7.3 Video Player

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -11,28 +12,32 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   //값이 변할 거기 때문에 final로 안 함.
   int _itemCount = 4;
   final PageController _pageController = PageController();
-
-  List<Color> colors = [
-    Colors.blue,
-    Colors.red,
-    Colors.yellow,
-    Colors.teal,
-  ];
+  final Duration _scrollDuration = const Duration(milliseconds: 10);
+  final Curve _scrollCurve = Curves.linear;
 
   void _onPageChanged(int page) {
-      _pageController.animateToPage(page,
-          duration: const Duration(milliseconds: 10), curve: Curves.linear);
+    _pageController.animateToPage(
+      page,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
     if (page == _itemCount - 1) {
       _itemCount = _itemCount + 4;
-      //add all = 한 리스트에 있는 모든 항목들을 다른 리스트에 추가한다.
-      colors.addAll([
-        Colors.blue,
-        Colors.red,
-        Colors.yellow,
-        Colors.teal,
-      ]);
       setState(() {});
     }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -42,15 +47,9 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemCount: _itemCount,
-      itemBuilder: (context, index) => Container(
-        color: colors[index],
-        child: Center(
-          child: Text(
-            'screen $index',
-            style: const TextStyle(fontSize: 55),
-          ),
-        ),
-      ),
+      itemBuilder: (context, index) =>
+          VideoPost(onVideoFinished: _onVideoFinished),
+      //함수를 video post로 넘겨줌. 근데 stateful에 넘겨주는 거지 state한테 가는 게 아님.
     );
   }
 }
--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPost extends StatefulWidget {
-  const VideoPost({super.key});
+  final Function onVideoFinished;
+
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
 }
 
 class _VideoPostState extends State<VideoPost> {
+  final VideoPlayerController _videoPlayerController =
+      VideoPlayerController.asset("assets/videos/video.mp4");
+
+
+void _onVideoChange(){
+if(_videoPlayerController.value.isInitialized){
+  if (
+    //영상 길이가 10초고, 현재 사용자의 영상 내 위치가 10초이면 영상이 끝난 걸로 본다. 
+    _videoPlayerController.value.duration ==_videoPlayerController.value.position
+  ) {
+    
+    widget.onVideoFinished();
+  }
+}
+}
+
+  void _initVideoPlayer() async {
+    //1단계:초기화.
+    await _videoPlayerController.initialize();
+
+    _videoPlayerController.play();
+    setState(() {});
+    _videoPlayerController.addListener(_onVideoChange );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+//statefulwidget의 프로퍼티를 state에서 접근.
+    widget.onVideoFinished();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        //fill = 화면 전체를 채우는 위젯 만들 때 사용.
+        Positioned.fill(
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Container(
+                    color: Colors.black,
+                  ))
+      ],
+    );
   }
 }
