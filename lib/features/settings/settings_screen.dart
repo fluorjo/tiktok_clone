d9994bba8729ffaa51d4054888276d8fd01b55e3
fluorjo <fluorjo@naver.com>
2023-03-01 04:53:10 +0900

    13.3 SwitchListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,13 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Settings',
+        ),
+      ),
       body: ListView(
         children: [
+          Switch.adaptive(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          CupertinoSwitch(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          Switch(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          SwitchListTile.adaptive(
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+            title: const Text(
+              'Enable notifications',
+            ),
+            subtitle: const Text(
+              'subtitle',
+            ),
+          ),
+          Checkbox(
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          CheckboxListTile(
+            checkColor: Colors.white,
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+            title: const Text(
+              'Enable notifications',
+            ),
+          ),
           ListTile(
             onTap: () async {
               final date = await showDatePicker(
