c60802b48c8a39839151e6e031b1b6e877411acc
fluorjo <fluorjo@naver.com>
2023-03-01 06:30:01 +0900

    14.1 OrientationBuilder

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -23,69 +23,109 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: Sizes.size40,
-          ),
-          child: Column(
-            children: [
-              Gaps.v80,
-              const Text(
-                'Sign up for TikTok',
-                style: TextStyle(
-                  fontSize: Sizes.size24,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              Gaps.v20,
-              const Text(
-                "Create a profile, follow other accounts, make your own videos, and more.",
-                style: TextStyle(
-                  fontSize: Sizes.size16,
-                  color: Colors.black45,
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              Gaps.v40,
-              GestureDetector(
-                onTap: () => _onEmailTap(context),
-                child: const AuthButton(
-                    icon: FaIcon(FontAwesomeIcons.user),
-                    text: "Use email & password"),
-              ),
-              Gaps.v16,
-              const AuthButton(
-                  icon: FaIcon(FontAwesomeIcons.apple),
-                  text: "Continue with Apple"),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-          color: Colors.grey.shade50,
-          elevation: 2,
+    return OrientationBuilder(builder: (
+      BuildContext context,
+      //orientation=디바이스 방향을 보여줌.
+      //portrait면 세로, landscape면 가로.
+      Orientation orientation,
+    ) {
+      //이렇게 회전했을 때 경고 화면만 띄우는 방법도 있긴 함.
+      /* if (orientation == Orientation.landscape) {
+          return const Scaffold(
+            body: Center(
+              child: Text('Plz rotate ur phone.'),
+            ),
+          );
+        } */
+      return Scaffold(
+        body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size40,
+            ),
+            child: Column(
               children: [
-                const Text('Already have an account?'),
-                Gaps.h5,
-                GestureDetector(
-                  onTap: () => _onLoginTap(context),
-                  child: Text(
-                    'Log in',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: Theme.of(context).primaryColor),
+                Gaps.v80,
+                const Text(
+                  'Sign up for TikTok',
+                  style: TextStyle(
+                    fontSize: Sizes.size24,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
+                Gaps.v20,
+                const Text(
+                  "Create a profile, follow other accounts, make your own videos, and more.",
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                    color: Colors.black45,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Gaps.v40,
+                if (orientation == Orientation.portrait) ...
+                  //...은 바로 밑 리스트에 있는 것들을 꺼내서 위 조건문을 전부 적용시키는 기능.
+                  [
+                  GestureDetector(
+                    onTap: () => _onEmailTap(context),
+                    child: const AuthButton(
+                        icon: FaIcon(FontAwesomeIcons.user),
+                        text: "Use email & password"),
+                  ),
+                  Gaps.v16,
+                  const AuthButton(
+                    icon: FaIcon(FontAwesomeIcons.apple),
+                    text: "Continue with Apple",
+                  ),
+                ],
+                if (orientation == Orientation.landscape)
+                  Row(
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () => _onEmailTap(context),
+                          child: const AuthButton(
+                              icon: FaIcon(FontAwesomeIcons.user),
+                              text: "Use email & password"),
+                        ),
+                      ),
+                      Gaps.h16,
+                      const Expanded(
+                        child: AuthButton(
+                          icon: FaIcon(FontAwesomeIcons.apple),
+                          text: "Continue with Apple",
+                        ),
+                      ),
+                    ],
+                  ),
               ],
             ),
-          )),
-    );
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+            color: Colors.grey.shade50,
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Already have an account?'),
+                  Gaps.h5,
+                  GestureDetector(
+                    onTap: () => _onLoginTap(context),
+                    child: Text(
+                      'Log in',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                ],
+              ),
+            )),
+      );
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/login_form_screen.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
@@ -10,7 +11,15 @@ import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dar
 import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 import 'package:tiktok_clone/features/settings/settings_screen.dart';
 
-void main() {
+void main() async {
+  //위젯과 엔진의 연결. 앱을 실행하기 전에 bind, widget, engine을 초기화하고 연결함.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  );
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
   runApp(const TikTokApp());
 }
 
@@ -20,16 +29,17 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xFFE9435A),
-        textSelectionTheme:
-            const TextSelectionThemeData(cursorColor: Color(0xFFE9435A),
-             //selectionColor: Color(0xFFE9435A),
-            ),
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(0xFFE9435A),
+          //selectionColor: Color(0xFFE9435A),
+        ),
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
         appBarTheme: const AppBarTheme(
           centerTitle: true,
           foregroundColor: Colors.black,
@@ -41,9 +51,9 @@ class TikTokApp extends StatelessWidget {
               fontWeight: FontWeight.w600),
         ),
       ),
-      home: const SettingsScreen(),
+      //home: const SettingsScreen(),
       //home: const ChatDetailScreen(),
-      // home: const SignUpScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
