7a696c186f7eb81a33348932a6b071105d9e843d
fluorjo <fluorjo@naver.com>
2023-03-01 03:44:08 +0900

    13.2 showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -8,21 +8,43 @@ class SettingsScreen extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          // ListTile(
-          //   onTap: () => showAboutDialog(
-          //     context: context,
-          //     applicationVersion: '1.0',
-          //     applicationLegalese: 'All rights reserved. Please do not copy me',
-          //   ),
-          //   title: const Text(
-          //     'About',
-          //     style: TextStyle(
-          //       fontWeight: FontWeight.w600,
-          //     ),
-          //   ),
-          //   subtitle: const Text('About this app...'),
-          // ),
-          AboutListTile(),
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1900),
+                lastDate: DateTime(2030),
+              );
+              print(date);
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1999),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
+            title: const Text(
+              'What is your birthday?',
+            ),
+          ),
+          const AboutListTile(),
         ],
       ),
     );
