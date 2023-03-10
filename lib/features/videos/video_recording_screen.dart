3dcba61adbb38079799b137402c798c854da40cb
fluorjo <fluorjo@naver.com>
2023-03-11 04:17:16 +0900

    19.5 Recording Animation

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -11,9 +11,28 @@ class VideoRecordingScreen extends StatefulWidget {
   State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
 }
 
-class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+class _VideoRecordingScreenState extends State<VideoRecordingScreen>
+    with TickerProviderStateMixin {
   bool _hasPermission = false;
   bool _isSelfieMode = false;
+  late final AnimationController _buttonAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 150),
+  );
+
+  late final Animation<double> _buttonAnimation = Tween(
+    begin: 1.0,
+    end: 1.3,
+  ).animate(_buttonAnimationController);
+
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 10),
+    lowerBound: 0.0,
+    upperBound: 1.0,
+  );
   late FlashMode _flashMode;
   late CameraController _cameraController;
 
@@ -56,6 +75,14 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     // TODO: implement initState
     super.initState();
     initPermissions();
+    _progressAnimationController.addListener(() {
+      setState(() {});
+    });
+    _progressAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _stopRecording();
+      }
+    });
   }
 
   Future<void> _toggleSelfieMode() async {
@@ -68,6 +95,21 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     await _cameraController.setFlashMode(newFlashMode);
     _flashMode = newFlashMode;
     setState(() {});
+    _progressAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _stopRecording();
+      }
+    });
+  }
+
+  void _startRecording(TapDownDetails _) {
+    _buttonAnimationController.forward();
+    _progressAnimationController.forward();
+  }
+
+  void _stopRecording() {
+    _buttonAnimationController.reverse();
+    _progressAnimationController.reset();
   }
 
   @override
@@ -111,32 +153,72 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
                             icon: const Icon(
                               Icons.flash_off_rounded,
                             ),
-                            color: _flashMode == FlashMode.off ? Colors.yellow : Colors.white,
+                            color: _flashMode == FlashMode.off
+                                ? Colors.yellow
+                                : Colors.white,
                           ),
                           IconButton(
                             onPressed: () => _setFlashMode(FlashMode.always),
                             icon: const Icon(
                               Icons.flash_on_rounded,
                             ),
-                            color: _flashMode == FlashMode.always ? Colors.yellow : Colors.white,
+                            color: _flashMode == FlashMode.always
+                                ? Colors.yellow
+                                : Colors.white,
                           ),
                           IconButton(
                             onPressed: () => _setFlashMode(FlashMode.auto),
                             icon: const Icon(
                               Icons.flash_auto_rounded,
                             ),
-                            color: _flashMode == FlashMode.auto ? Colors.yellow : Colors.white,
+                            color: _flashMode == FlashMode.auto
+                                ? Colors.yellow
+                                : Colors.white,
                           ),
                           IconButton(
                             onPressed: () => _setFlashMode(FlashMode.torch),
                             icon: const Icon(
                               Icons.flashlight_on_rounded,
                             ),
-                            color: _flashMode == FlashMode.torch ? Colors.yellow : Colors.white,
+                            color: _flashMode == FlashMode.torch
+                                ? Colors.yellow
+                                : Colors.white,
                           ),
                         ],
                       ),
                     ),
+                    Positioned(
+                      bottom: Sizes.size40,
+                      child: GestureDetector(
+                        onTapDown: _startRecording,
+                        onTapUp: (details) => _stopRecording(),
+                        child: ScaleTransition(
+                          scale: _buttonAnimation,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              SizedBox(
+                                width: Sizes.size80 + Sizes.size14,
+                                height: Sizes.size80 + Sizes.size14,
+                                child: CircularProgressIndicator(
+                                  color: Colors.red.shade400,
+                                  strokeWidth: Sizes.size6,
+                                  value: _progressAnimationController.value,
+                                ),
+                              ),
+                              Container(
+                                width: Sizes.size80,
+                                height: Sizes.size80,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.red.shade400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 ),
         ));
