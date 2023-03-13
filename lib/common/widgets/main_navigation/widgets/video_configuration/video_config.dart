b40ee98d817d907352f3d53bd2ca457a8fe45958
fluorjo <fluorjo@naver.com>
2023-03-14 05:46:35 +0900

    20.7 InheritedWidget part Two

--- lib/common/widgets/main_navigation/widgets/video_configuration/video_config.dart
+++ lib/common/widgets/main_navigation/widgets/video_configuration/video_config.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 
-class VideoConfig extends InheritedWidget {
+class VideoConfigData extends InheritedWidget {
   //InheritedWidget를 쓰고 이걸 extends할 때는 child가 필요하다. 중요!
-  const VideoConfig({super.key, required super.child});
+  const VideoConfigData({super.key, required super.child});
 
-  final bool autoMute = true;
-  static VideoConfig of(BuildContext context){
-    return context.dependOnInheritedWidgetOfExactType<VideoConfig>()!;
+  static VideoConfigData of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<VideoConfigData>()!;
   }
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
     return true;
+  }
+}
+
+class VideoConfig extends StatefulWidget {
+  final Widget child;
+
+  const VideoConfig({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  State<VideoConfig> createState() => _VideoConfigState();
+}
+
+class _VideoConfigState extends State<VideoConfig> {
+  bool autoMute = true;
+
+  void toggleMuted() {
+    setState(() {
+      autoMute = !autoMute;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoConfigData(
+      child: widget.child,
+    );
   }
 }
