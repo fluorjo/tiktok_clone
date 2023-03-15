e2eea6933806bd6b2de984576aafbc32dbeab4fe
fluorjo <fluorjo@naver.com>
2023-03-15 21:28:15 +0900

    21.2 VideoPlaybackConfigRepository

---
+++ lib/features/videos/repos/playback_config_repo.dart
@@ -0,0 +1,27 @@
+//오로지 데이터의 유지와 읽기만 책임지는 파일.
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class VideoPlaybackConfigRepository {
+  static const String _autoplay = "autoplay";
+  static const String _muted = "muted";
+
+  SharedPreferences _preferences;
+
+  VideoPlaybackConfigRepository(this._preferences);
+
+  Future<void> setMuted(bool value) async {
+    _preferences.setBool(_muted, value);
+  }
+  Future<void> setAutoplay(bool value) async {
+    _preferences.setBool(_autoplay, value);
+  }
+
+  bool isMuted(){
+    return _preferences.getBool(_muted) ?? false;
+  }
+  bool isAutoplay(){
+    return _preferences.getBool(_autoplay) ?? false;
+  }
+}
---
+++ lib/features/videos/view_models/playback_config_vm.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class PlaybackConfigModelViewModel extends ChangeNotifier {
+
+}
