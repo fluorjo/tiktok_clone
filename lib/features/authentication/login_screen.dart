ac90c7abfaf75ccbd945a41cf31352d881e904cf
fluorjo <fluorjo@naver.com>
2023-03-23 06:40:10 +0900

    24.4 Github Login

--- lib/features/authentication/login_screen.dart
+++ lib/features/authentication/login_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/login_form_screen.dart';
+import 'package:tiktok_clone/features/authentication/view_models/social_auth_view_model.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 import 'package:tiktok_clone/generated/l10n.dart';
 import 'package:tiktok_clone/utils.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   static String routeName = "login";
   static String routeURL = "/login";
 
@@ -28,7 +30,7 @@ class LoginScreen extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -64,9 +66,12 @@ class LoginScreen extends StatelessWidget {
                     text: S.of(context).emailPasswordButton),
               ),
               Gaps.v16,
-              AuthButton(
-                  icon: const FaIcon(FontAwesomeIcons.apple),
-                  text: S.of(context).appleButton),
+              GestureDetector(
+                onTap: ()=> ref.read(socialAuthProvider.notifier).githubSignIn(context),
+                child: AuthButton(
+                    icon: FaIcon(FontAwesomeIcons.github),
+                    text: S.of(context).appleButton),
+              ),
             ],
           ),
         ),
