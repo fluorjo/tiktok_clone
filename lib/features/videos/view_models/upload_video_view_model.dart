6b1582139b6ad360f274971e9afdb9e6ffb3feda
fluorjo <fluorjo@naver.com>
2023-04-03 06:39:55 +0900

    26.6 Conclusions

--- lib/features/videos/view_models/upload_video_view_model.dart
+++ lib/features/videos/view_models/upload_video_view_model.dart
@@ -39,7 +39,8 @@ class UploadVideoViewModel extends AsyncNotifier<void> {
             title: 'From Flutter',
             creator: userProfile.name,
           ),);
-          context.pushReplacement("/home");
+          context.pop();
+          context.pop();
         }
       });
     }
