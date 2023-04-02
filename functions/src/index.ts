2e91703d2d5528673d50fa1a49669d74baf4900c
fluorjo <fluorjo@naver.com>
2023-04-03 06:30:21 +0900

    26.5 publicUrl

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -24,8 +24,16 @@ export const onVideoCreated = functions.firestore
     ]);
     const storage = admin.storage();
     //여기 파일명 같아야 함. 중요. 
-    await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`,{
+    const [file, _]= await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`,{
         destination:`thumbnails/${snapshot.id}.jpg`
     });
+    await file.makePublic();
+    await snapshot.ref.update({'thumbnailUrl':file.publicUrl(),});
+    const db=admin.firestore();
+
+    await db.collection("users").doc(video.creatorUid).collection('videos').doc(snapshot.id).set({
+        thumbnailUrl: file.publicUrl(),
+        videoId:snapshot.id
+    })
 });
 
