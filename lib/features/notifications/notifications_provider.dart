80e218e8b26680b681cae574420cda0e2712d016
fluorjo <fluorjo@naver.com>
2023-04-13 12:14:22 +0900

    29.4 ShellRoute

--- lib/features/notifications/notifications_provider.dart
+++ lib/features/notifications/notifications_provider.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 
-class NotificationProvider extends AsyncNotifier {
+class NotificationProvider extends FamilyAsyncNotifier<void, BuildContext> {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
   final FirebaseMessaging _messaging = FirebaseMessaging.instance;
 
@@ -14,7 +16,7 @@ class NotificationProvider extends AsyncNotifier {
     await _db.collection('users').doc(user!.uid).update({"token": token});
   }
 
-  Future<void> initListeners() async {
+  Future<void> initListeners(BuildContext context) async {
     final permission = await _messaging.requestPermission();
     if (permission.authorizationStatus == AuthorizationStatus.denied) {
       return;
@@ -24,21 +26,21 @@ class NotificationProvider extends AsyncNotifier {
       print(event.notification?.title);
     });
     FirebaseMessaging.onMessageOpenedApp.listen((notification) {
-      print(notification.data['screen']);
+    context.go('/chats');
     });
     final notification = await _messaging.getInitialMessage();
     if(notification!=null){
-
+      context.go('/settings');
       print(notification.data['screen']);
     }
   }
 
   @override
-  FutureOr build() async {
+  FutureOr build(BuildContext context) async {
     final token = await _messaging.getToken();
     if (token == null) return;
     await updateToken(token);
-    await initListeners();
+    await initListeners(context);
     _messaging.onTokenRefresh.listen((newToken) async {
       await updateToken(newToken);
     });
